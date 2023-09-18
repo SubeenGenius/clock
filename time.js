3568303bfcef865756d9623aa6fff95d5f99821c
Subeen Bohara <142922234+SubeenGenius@users.noreply.github.com>
2023-09-18 16:49:12 +0545

    Add files via upload

---
+++ time.js
@@ -0,0 +1,30 @@
+function updateCountdown() {
+    const targetDate = new Date('2023-10-22T00:00:00');
+    const currentDate = new Date();
+
+    const timeDifference = targetDate - currentDate;
+
+    if (timeDifference <= 0) {
+        document.getElementById('days').textContent = '00';
+        document.getElementById('hours').textContent = '00';
+        document.getElementById('minutes').textContent = '00';
+        document.getElementById('seconds').textContent = '00';
+        clearInterval(interval);
+    } else {
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        document.getElementById('days').textContent = days.toString().padStart(2, '0');
+        document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+        document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+        document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+    }
+}
+
+// Initial call to update the countdown
+updateCountdown();
+
+// Update the countdown every second
+const interval = setInterval(updateCountdown, 1000);
